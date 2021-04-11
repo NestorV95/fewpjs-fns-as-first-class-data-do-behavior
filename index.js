8427dc185073a776a8ca79cc7a2f99c3138e3c16
Nestor <nestor.venegas95@gmail.com>
2021-04-10 21:28:05 -0500

    commit tree c8361663c89f44286dfb6a8ecaf23a55292eacbb

--- index.js
+++ index.js
@@ -6,8 +6,21 @@ function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
-
 /* End Given Code, don't edit above here...*/
-
 /* Write your implementation of greet() */
+function greet(string){
+  const time = parseInt(string)
+    if(time < 12){
+      return 'Good Morning'
+    }if(time > 17){
+      return 'Good Evening'
+    }else{
+      return 'Good Afternoon'
+    }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+  const greeting = document.querySelector('#greeting')
+  greeting.innerText = string
+}
